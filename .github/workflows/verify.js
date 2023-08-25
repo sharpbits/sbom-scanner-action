4b4c6f90686981208b974aa4d827e5726a111494
Ken Hill <kennethjhill@gmail.com>
2023-08-25 08:37:49 -0400

    Verify test output

---
+++ .github/workflows/verify.js
@@ -0,0 +1,17 @@
+import * as fs from 'fs'
+
+const sbomJson = fs.readFileSync('sbom.json')
+const sbom = JSON.parse(sbomJson)
+
+if (!sbom || !sbom.scan_date || !sbom.orgs?.length == 1) {
+  throw new Error('Failed to parse SBOM JSON')
+}
+
+const entry = sbom.repos['sbom-scanner-action']
+if (!entry || !entry?.manifest?.github?.master_status?.commit_sha
+           || entry?.manifest?.manifest?.project_name !== 'SBOM Scanner Action'
+           || entry?.manifest?.dockerfile?.base_image !== 'node') {
+  throw new Error('Failed to verify data from SBOM')
+}
+
+console.log('Test output verified')
